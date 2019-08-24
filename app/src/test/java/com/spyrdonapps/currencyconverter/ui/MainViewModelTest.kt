5ee5732b3edcc72fe731dfde2debb410b5d341eb
patrycjusz.blaszczuk <patrycjusz.blaszczuk@gmail.com>
2019-08-24 16:31:25 +0200

    Tests somewhat better

--- app/src/test/java/com/spyrdonapps/currencyconverter/ui/MainViewModelTest.kt
+++ app/src/test/java/com/spyrdonapps/currencyconverter/ui/MainViewModelTest.kt
@@ -1,36 +1,23 @@
 package com.spyrdonapps.currencyconverter.ui
 
-import androidx.lifecycle.Observer
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.argumentCaptor
-import com.nhaarman.mockitokotlin2.isNull
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
 import com.spyrdonapps.currencyconverter.data.model.Currency
 import com.spyrdonapps.currencyconverter.data.repository.CurrencyRepository
 import com.spyrdonapps.currencyconverter.test.data.CurrenciesTestData
 import com.spyrdonapps.currencyconverter.test.util.InstantTaskExecutorRule
+import com.spyrdonapps.currencyconverter.test.util.captureValues
 import com.spyrdonapps.currencyconverter.util.state.Result
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
 import org.junit.After
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.reset
-import org.mockito.Mockito.times
-import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 import java.io.IOException
 
@@ -51,8 +38,6 @@ class MainViewModelTest {
 
     private val mockCurrencyRepository: CurrencyRepository = mock()
 
-    private var observer: Observer<Result<List<Currency>>> = mock()
-
     // endregion helper fields
 
     private lateinit var classUnderTest: MainViewModel
@@ -61,62 +46,51 @@ class MainViewModelTest {
     fun setup() {
         Dispatchers.setMain(testDispatcher)
         classUnderTest = MainViewModel(mockCurrencyRepository)
-        classUnderTest.currenciesLiveData.observeForever(observer)
     }
 
     @After
     fun tearDown() {
         Dispatchers.resetMain()
-//        reset(observer, mockCurrencyRepository)
-//        classUnderTest.currenciesLiveData.removeObserver(observer)
     }
 
-    // TODO always works when ran alone, always fails when testing whole class, some race conditions occur
+    // TODO sometimes returns correct data, some race conditions occur
     @Test
-    fun mainViewModel_currenciesLiveDataHadDataLoadingStatePosted() {
-        argumentCaptor<Result<List<Currency>>>().run {
-            verify(observer, times(2)).onChanged(capture())
-
-            assertThat(firstValue, `is`(instanceOf(Result.Loading::class.java)))
+    fun `mainViewModel, remote data not available and cached data not available, currenciesLiveData had error state`() {
+        runBlockingTest {
+            val exception = IOException()
+            `when`(mockCurrencyRepository.getCurrenciesFromRemote()).thenAnswer { throw exception }
+            `when`(mockCurrencyRepository.getCurrenciesFromCache()).thenAnswer { emptyList<List<Currency>>() }
+            classUnderTest.currenciesLiveData.captureValues {
+                assertSendsValues(2000, Result.Loading, Result.Error(exception))
+            }
         }
     }
 
     // TODO sometimes returns correct data, some race conditions occur
     // Also works when viewmodel is created right in arrange of this test
     @Test
-    fun mainViewModel_currenciesLiveDataInteractedTwice() {
-        verify(observer, times(2)).onChanged(any())
+    fun `mainViewModel, remote data available, currenciesLiveData had success state with correct data`() {
+        runBlockingTest {
+            `when`(mockCurrencyRepository.getCurrenciesFromRemote()).thenAnswer { CurrenciesTestData.currencies }
+            classUnderTest.currenciesLiveData.captureValues {
+                assertSendsValues(2000, Result.Loading, Result.Success(CurrenciesTestData.currencies))
+            }
+        }
     }
 
     // TODO sometimes returns correct data, some race conditions occur
-    @Test
-    fun mainViewModel_remoteDataAvailable_currenciesLiveDataHadSuccessStateWithCorrectData() {
-        runBlockingTest {
-            `when`(mockCurrencyRepository.getCurrenciesFromRemote()).thenAnswer { CurrenciesTestData.currencies }
-            delay(1000)
-        }
-        argumentCaptor<Result<List<Currency>>>().run {
-            verify(observer, times(2)).onChanged(capture())
-
-            assertThat(lastValue, `is`(Result.Success(CurrenciesTestData.currencies) as Result<List<Currency>>))
-        }
-    }
-
-    // TODO doesn't work now, TOTALLY RANDOM error or success + probably some race conditions occur
-    @Test
-    fun `mainViewModel, remote data not available and cached data not available, currenciesLiveData had error state`() {
-        runBlockingTest {
-            `when`(mockCurrencyRepository.getCurrenciesFromRemote()).thenAnswer { throw IOException() }
-            `when`(mockCurrencyRepository.getCurrenciesFromCache()).thenAnswer { emptyList<List<Currency>>() }
-            delay(1000)
-        }
-        argumentCaptor<Result<List<Currency>>>().run {
-            verify(observer, times(2)).onChanged(capture())
-
-            assertThat(lastValue, `is`(instanceOf(Result.Error::class.java)))
-            assertEquals(null, (lastValue as Result.Error<List<Currency>>).data)
-        }
-    }
+//    @Test
+//    fun mainViewModel_remoteDataAvailable_currenciesLiveDataHadSuccessStateWithCorrectData() {
+//        runBlockingTest {
+//            `when`(mockCurrencyRepository.getCurrenciesFromRemote()).thenAnswer { CurrenciesTestData.currencies }
+//            delay(1000)
+//        }
+//        argumentCaptor<Result<List<Currency>>>().run {
+//            verify(observer, times(2)).onChanged(capture())
+//
+//            assertThat(lastValue, `is`(Result.Success(CurrenciesTestData.currencies) as Result<List<Currency>>))
+//        }
+//    }
 
     // region helper methods
 
